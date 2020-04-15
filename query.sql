553b9fed8073dc8f6c0623e9f30ed02dd6ba8e90
Kseniia <44757008+KsyuYu@users.noreply.github.com>
2020-04-15 21:35:30 +0300

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,32 @@
+---QUERY 1
+SELECT
+    TotalCases.country,
+    TotalCases.total_cases
+ FROM
+    TotalCases
+ORDER BY
+    total_cases DESC;
+
+---QUERY 2	
+SELECT
+    TotalCases.country,
+	round((TotalCases.total_cases + 0.0) * 100 / t.total, 2) as persent
+FROM
+    TotalCases,
+    (SELECT
+        SUM(TotalCases.total_cases) AS total
+     FROM
+        TotalCases
+    ) t
+GROUP BY
+    TotalCases.country,
+    t.total;
+ 
+ ---QUERY 3
+ SELECT
+    TotalCases.total_cases,
+	TotalDeaths.total_deaths
+ FROM
+    TotalCases INNER JOIN TotalDeaths ON TotalCases.country = TotalDeaths.country
+ORDER BY
+    total_cases DESC;
