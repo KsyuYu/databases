183d902d51a6e5ba635cfb0f7441d4bb8e0b5f2c
Kseniia <44757008+KsyuYu@users.noreply.github.com>
2020-04-15 19:07:43 +0300

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,35 @@
+-- POPULATE Rank
+INSERT INTO Rank (total_cases, rank) VALUES (502876, 1);
+INSERT INTO Rank (total_cases, rank) VALUES (158273, 2);
+INSERT INTO Rank (total_cases, rank) VALUES (147577, 3);
+INSERT INTO Rank (total_cases, rank) VALUES (124869, 4);
+INSERT INTO Rank (total_cases, rank) VALUES (122171, 5);
+INSERT INTO Rank (total_cases, rank) VALUES (81907, 6);
+INSERT INTO Rank (total_cases, rank) VALUES (73758, 7);
+INSERT INTO Rank (total_cases, rank) VALUES (68192, 8);
+INSERT INTO Rank (total_cases, rank) VALUES (47029, 9);
+INSERT INTO Rank (total_cases, rank) VALUES (26667, 10);
+
+-- POPULATE TotalCases
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('USA', 502876, 33752);
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('Spain', 158273, 5051);
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('Italy', 147577, 3951);
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('France', 124869, 7120);
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('Germany', 122171, 3936);
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('China', 81907, 42);
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('UK', 73758, 8681);
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('Iran', 68192, 1972);
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('Turkey', 47029, 4747);
+INSERT INTO TotalCases (country, total_cases, new_cases) VALUES ('Belgium', 26667, 1684);
+
+-- POPULATE TotalDeaths
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('USA', 18747, 2035);
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('Spain', 16081, 634);
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('Italy', 18849, 570);
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('France', 13197, 987);
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('Germany', 2736, 129);
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('China', 3336, 1);
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('UK', 8958, 980);
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('Iran', 4232, 122);
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('Turkey', 1006, 98);
+INSERT INTO TotalDeaths (country, total_deaths, new_deaths) VALUES ('Belgium', 3019, 496);
